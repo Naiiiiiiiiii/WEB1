8f3b27c66f2c3ebf9a46cb26928860f594e557bc
Naiiiiiiiiiii <emkotenrr@gmail.com>
2025-09-29 12:24:07 +0700

    sua main

---
+++ js/products.js
@@ -0,0 +1,68 @@
+const products = [
+  {
+    id: 1,
+    name: "Giày thể thao CA Match",
+    category: "thethao",
+    price: 2300000,
+    oldPrice: 2400000,
+    img: "./img/giaythethao_CAMatch.avif",
+    rating: 5,
+    ratingCount: 128,
+    badge: "hot"
+  },
+  {
+    id: 2,
+    name: "Giày thể thao Suede Classic Unisex",
+    category: "thethao",
+    price: 1840000,
+    oldPrice: 2300000,
+    img: "./img/Giày-thể-thao-Suede-Classic-Unisex.avif",
+    rating: 4,
+    ratingCount: 95,
+    badge: "sale"
+  },
+  {
+    id: 3,
+    name: "GIÀY DA CÔNG SỞ (DA THẬT) - GERMANO BELLESI - SẢN XUẤT THỦ CÔNG TẠI ITALY",
+    category: "congso",
+    price: 10990000,
+    oldPrice: null,
+    img: "./img/giaycongsoGERMANO.webp",
+    rating: 5,
+    ratingCount: 203,
+    badge: "new"
+  },
+  {
+    id: 4,
+    name: "Giày Boots nam MATURE Chelsea Boots – Đen – Version 2025",
+    category: "congso",
+    price: 1399000,
+    oldPrice: null,
+    img: "./img/bootsnam.webp",
+    rating: 4,
+    ratingCount: 76,
+    badge: null
+  },
+  {
+    id: 5,
+    name: "Giày Sneaker nam DYNAMIC – Vàng bò – Version 2025",
+    category: "casual",
+    price: 1399000,
+    oldPrice: null,
+    img: "./img/casual_Dynamic.webp",
+    rating: 5,
+    ratingCount: 156,
+    badge: null
+  },
+  {
+    id: 6,
+    name: "Giày nam Warrior 2025 phong cách trẻ trung",
+    category: "casual",
+    price: 522500,
+    oldPrice: 550000,
+    img: "./img/casual_Warrior.png",
+    rating: 4,
+    ratingCount: 89,
+    badge: "sale"
+  }
+];
